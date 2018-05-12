7f7f0ffff088c5b30947b44de27f4dd6450b8e04
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-05-12 18:03:01 -0400

    with @Autowired on any FIELD  can also be  used for dependency  injection

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 	
+	@Autowired
 	private FortuneService fortuneService;
 	
 	// define a default constructor
@@ -13,18 +14,21 @@ public class TennisCoach implements Coach {
 		System.out.println(">> TennisCoach: inside default constructor");
 	}
 	
-	
+	/*
 	@Autowired
 	public void doSomeCrazyStuff(FortuneService theFortuneService) {
 		System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
 		this.fortuneService = theFortuneService;
 	}
+	*/
 	
-	/*@Autowired
+	/*
+	@Autowired
 	public TennisCoach(FortuneService theFortuneService) {
 		super();
 		this.fortuneService = theFortuneService;
-	}*/
+	}
+	*/
 
 	@Override
 	public String getDailyWorkout() {
