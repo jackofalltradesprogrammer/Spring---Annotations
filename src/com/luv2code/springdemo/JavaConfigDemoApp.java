d0e25f89799cb3223807672394378c4f12dc8255
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-05-16 14:56:21 -0400

    Some updates:

--- src/com/luv2code/springdemo/JavaConfigDemoApp.java
+++ src/com/luv2code/springdemo/JavaConfigDemoApp.java
@@ -1,7 +1,6 @@
 package com.luv2code.springdemo;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class JavaConfigDemoApp {
 
